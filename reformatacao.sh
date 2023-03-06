39c271a18ab9405386f060b7c2bbce1833cf7c9b
brunopiato <piatobio@gmail.com>
2023-03-06 13:57:47 -0300

    subindo arquivo bash de reformatacao do linux

---
+++ reformatacao.sh
@@ -0,0 +1,62 @@
+# Arquivo de configuração do sistema após formatação
+: << 'COMMENT'
+Este arquivo serve como 
+automatizacao do processo 
+pos-instalacao e formatacao
+do OS com Ubuntu.
+COMMENT
+
+# Atualizando o sistema
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+
+# Instalando o Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+
+# Instalando as extensões do Linux
+sudo apt install gnome-tweaks
+sudo apt isntall gnome-shell-extension
+sudo apt install chrome-gnome-shell
+
+# Instalando o VSCode Insiders
+sudo snap install code-insiders --classic
+
+# Instalando aplicativos do Snap
+sudo snap install drawio #Drawio
+sudo snap install spotify #Spotify
+sudo snap install dbeaver-ce #DBeaver
+sudo snap install discord #Discord
+sudo snap install inkscape #Inkscape
+sudo snap install notion-snap #Notion
+sudo snap install  #Dropbox
+sudo snap install notion-snap #Notion
+
+# Instalando e configurando o Calibre
+sudo apt-get install -y calibre
+
+sudo nano /etc/profile.d/calibre.sh
+echo "export CALIBRE_USE_DARK_PALETTE=1" >> /etc/profile.d/calibre.sh
+
+# Instalando o Dropbox
+# https://help.dropbox.com/pt-br/installs/linux-commands
+wget https://linux.dropbox.com/packages/nautilus-dropbox-2022.12.05.tar.bz2
+sudo apt install .//nautilus-dropbox-2022.12.05.tar.bz2
+
+# Instalando o pyenv e o Python
+
+
+# Instalando o R 
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/cran.gpg
+echo deb [signed-by=/usr/share/keyrings/cran.gpg] https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/ | sudo tee /etc/apt/sources.list.d/cran.list
+sudo apt update
+sudo apt install r-base
+R --version
+
+
+# Instalando Git
+sudo apt update
+sudo apt install git

ca17dd4570b19acde473873b0de60ee3795f7ebb
brunopiato <piatobio@gmail.com>
2023-03-06 14:14:42 -0300

    arrumando o arquivo de reformatacao do linux

--- reformatacao.sh
+++ reformatacao.sh
@@ -14,7 +14,6 @@ sudo apt install curl
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
-
 # Instalando as extensões do Linux
 sudo apt install gnome-tweaks
 sudo apt isntall gnome-shell-extension
@@ -44,9 +43,31 @@ echo "export CALIBRE_USE_DARK_PALETTE=1" >> /etc/profile.d/calibre.sh
 wget https://linux.dropbox.com/packages/nautilus-dropbox-2022.12.05.tar.bz2
 sudo apt install .//nautilus-dropbox-2022.12.05.tar.bz2
 
+#-----------------------------------------------------------------------------------------
+# Instalando Git
+sudo apt update
+sudo apt install git
+
+
+#-----------------------------------------------------------------------------------------
 # Instalando o pyenv e o Python
+## Instalando o pyenv
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm gettext libncurses5-dev tk-dev tcl-dev blt-dev libgdbm-dev git python2-dev python3-dev aria2
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+sudo echo 'export PYTHON_BUILD_ARIA2_OPTS="-x 10 -k 1M"
+
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+## Instalando o gerenciador de pacotes do Python
+sudo apt install python3-pip
+pip install cython
 
 
+#-----------------------------------------------------------------------------------------
 # Instalando o R 
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
@@ -55,8 +76,3 @@ echo deb [signed-by=/usr/share/keyrings/cran.gpg] https://cloud.r-project.org/bi
 sudo apt update
 sudo apt install r-base
 R --version
-
-
-# Instalando Git
-sudo apt update
-sudo apt install git
